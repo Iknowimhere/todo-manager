9633ab72b33e29bdeb906b7c6ffaa4559d16827c
umashankar <umashankarp33@gmail.com>
2024-07-19 19:52:50 +0530

    cookies tutorial in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ dotenv.config()
 import { db } from "./config/db.js";
 import taskRouter from "./routes/taskRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 db()
 
 let app=express()
@@ -17,14 +18,40 @@ app.use(methodOverride('_method'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
-app.get("/",(req,res)=>{
-    res.render("index")
+// app.get("/",(req,res)=>{
+//     res.render("index")
+// })
+
+// app.use("/task",taskRouter);
+// app.use("/users",userRouter);
+
+// cookie
+
+app.get("/set-cookie",(req,res)=>{
+    res.cookie("user","shankar",{
+        maxAge:1000*60*60*24,
+        httpOnly:true
+    })
+    res.send("cookie set")
 })
 
-app.use("/task",taskRouter);
-app.use("/users",userRouter);
+app.get("/get-cookie",(req,res)=>{
+    res.send(`cookie is ${req.cookies.user}`)
+})
 
+app.get("/update-cookie",(req,res)=>{
+    res.cookie("user","stark",{
+        maxAge:1000*60*60*24,
+        httpOnly:true
+    })
+    res.send("cookie updated")
+})
 
+app.get("/delete-cookie",(req,res)=>{
+    res.clearCookie("user")
+    res.send("cookie deleted")
+})
 
 export default app;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getLoginPage, getSignupPage, login, signup } from "../controllers/userControllers.js";
-import { auth } from "../middlewares/auth.js";
+
 
 
 let userRouter=Router()
@@ -13,9 +13,9 @@ userRouter.post("/signup",signup)
 userRouter.get("/login",getLoginPage)
 userRouter.post("/login",login)
 
-userRouter.get("/home",auth,(req,res,next)=>{
-res.send(`Welcome to home page ${req.user.name}!`)
-})
+// userRouter.get("/home",auth,(req,res,next)=>{
+// res.send(`Welcome to home page ${req.user.name}!`)
+// })
     
 
 export default userRouter;
